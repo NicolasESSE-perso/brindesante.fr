ab527fa62fba70e8e163b9f899fe7cc22a71fda6
Nicolas <Nicolas@MacBook-Nie.local>
2020-04-17 21:16:23 +0200

    Mise en page de la page d'accueil

--- src/FicheM/FicheM.js
+++ src/FicheM/FicheM.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Style from "./FicheM.module.css";
 
-function FicheM(props) {
+function FicheM({ titre, description, date_modif, fiche_id }) {
   //commentaire
 
   return (
     <div className={Style.FicheM}>
-      <Link to={`/fiche/${props.fiche_id}`}>
-        <h1>{props.titre}</h1>
-        <p>{props.description}</p>
-        <p>{props.date_modif}</p>
+      <Link to={`/fiche/${fiche_id}`}>
+        <h1 className={Style.Titre}>{titre}</h1>
+        <p className={Style.Texte}>{description}</p>
+        <p className={Style.LireFiche}>Lire la suite</p>
+        <p>{date_modif}</p>
       </Link>
     </div>
   );
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,18 +1,29 @@
 import React, { useContext } from "react";
 import { ListeFichesContext } from "./ListeFichesContext";
 import FicheM from "./FicheM/FicheM";
+import Style from "./HomePage.module.css";
 
 function Homepage() {
   const [fiches] = useContext(ListeFichesContext);
 
   return (
-    <div className="HomePage">
-      <h1>
+    <div className={Style.HomePage}>
+      <h1 className={Style.Phrase}>
         "Des conseils sur les premiers soins à effectuer pour savoir quoi faire
         quand on ne sait plus quoi faire !"
       </h1>
+
+      <div className={Style.DivSousTitre}>
+        <p className={Style.SousTitre}>Symptomes les plus fréquents</p>
+      </div>
       {fiches.map((fiche) => (
-        <FicheM titre={fiche.titre} fiche_id={fiche._id} key={fiche._id} />
+        <FicheM
+          titre={fiche.titre}
+          fiche_id={fiche._id}
+          key={fiche._id}
+          description={fiche.description}
+          date_modif={fiche.date_modif}
+        />
       ))}
     </div>
   );
